7c2c6e28a1d1ec54be933bf459373da45e3ff8f5
karollb <73835249+karollb@users.noreply.github.com>
2021-08-02 14:42:25 +0200

    First Commit

---
+++ src/main/java/pl/edu/wszib/client_browser/ClientBrowserApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.client_browser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientBrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientBrowserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/wszib/client_browser/ClientBrowserApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.client_browser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientBrowserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
