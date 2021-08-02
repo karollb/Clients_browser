976a8c680c3fff0be51f6a2e5ded0ff4d2440e1a
karollb <73835249+karollb@users.noreply.github.com>
2021-08-02 15:33:00 +0200

    Client model

---
+++ src/main/java/pl/edu/wszib/client_browser/model/Client.java
@@ -0,0 +1,28 @@
+package pl.edu.wszib.client_browser.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String surname;
+    private String email;
+    private String address;
+    private String phone;
+    private String additionalInformation;
+
+
+}
